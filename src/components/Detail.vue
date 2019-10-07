a4d3a938ea3087f01d277a875082469498788849
TNT <347499586@qq.com>
2019-10-07 23:36:24 +0800

    wx初始化

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -74,37 +74,37 @@ export default {
   },
   methods: {
     share() {
-      if (typeof WeixinJSBridge === 'undefined') {
-        alert(' 请先通过微信搜索 wow36kr 添加36氪为好友，通过微信分享文章 :) ');
-      } else {
-        WeixinJSBridge &&
-          WeixinJSBridge.invoke('shareTimeline', {
-            title: '36氪',
-            link: 'http://www.36kr.com',
-            desc: ' 关注互联网创业 ',
-            img_url: 'http://www.36kr.com/assets/images/apple-touch-icon.png',
-          });
-      }
+      // if (typeof WeixinJSBridge === 'undefined') {
+      //   alert(' 请先通过微信搜索 wow36kr 添加36氪为好友，通过微信分享文章 :) ');
+      // } else {
+      //   WeixinJSBridge &&
+      //     WeixinJSBridge.invoke('shareTimeline', {
+      //       title: '36氪',
+      //       link: 'http://www.36kr.com',
+      //       desc: ' 关注互联网创业 ',
+      //       img_url: 'http://www.36kr.com/assets/images/apple-touch-icon.png',
+      //     });
+      // }
     },
     // 分享到朋友圈
     weixinShareTimeline(title, desc, link, imgUrl) {
-      WeixinJSBridge &&
-        WeixinJSBridge.invoke('shareTimeline', {
-          img_url: imgUrl,
-          link,
-          desc,
-          title,
-        });
+      // WeixinJSBridge &&
+      //   WeixinJSBridge.invoke('shareTimeline', {
+      //     img_url: imgUrl,
+      //     link,
+      //     desc,
+      //     title,
+      //   });
     },
     // 分享给朋友
     weixinSendAppMessage(title, desc, link, imgUrl) {
-      WeixinJSBridge &&
-        WeixinJSBridge.invoke('sendAppMessage', {
-          img_url: imgUrl,
-          link,
-          desc,
-          title,
-        });
+      // WeixinJSBridge &&
+      //   WeixinJSBridge.invoke('sendAppMessage', {
+      //     img_url: imgUrl,
+      //     link,
+      //     desc,
+      //     title,
+      //   });
     },
   },
 };
